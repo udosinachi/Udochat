d453e3a0f8fe1e4a2bf0f24e7c6c69724a212718
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2024-07-30 22:09:51 +0100

    Trending topics and hashtags

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,12 +1,24 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
-import Image from "next/image";
+import Post from "@/components/posts/Post";
+import TrendsSidebar from "@/components/TrendsSideBar";
+import prisma from "@/lib/prisma";
+import { PostDataInclude } from "@/lib/types";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    include: PostDataInclude,
+    orderBy: { createdAt: "desc" },
+  });
+
   return (
-    <main className="h-[200vh] w-full">
-      <div className="w-full">
+    <main className="flex w-full min-w-0 gap-5">
+      <div className="w-full min-w-0 space-y-5">
         <PostEditor />
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
       </div>
+      <TrendsSidebar />
     </main>
   );
 }
