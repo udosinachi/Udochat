79dadf932278117a766ca83d3c5b66318e3663a3
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2024-07-31 10:41:20 +0100

    Infinite scrolling with useInfiniteQuery

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -16,3 +16,8 @@ export const PostDataInclude = {
 export type PostData = Prisma.PostGetPayload<{
   include: typeof PostDataInclude;
 }>;
+
+export interface PostsPage {
+  posts: PostData[];
+  nextCursor: string | null;
+}
