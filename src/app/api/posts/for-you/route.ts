c42ab8ff57fbbbf8be5e21a92d13eec7f2eb1850
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2024-07-31 22:18:57 +0100

    Following of users is working

--- src/app/api/posts/for-you/route.ts
+++ src/app/api/posts/for-you/route.ts
@@ -1,6 +1,6 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
-import { PostDataInclude, PostsPage } from "@/lib/types";
+import { getPostDataInclude, PostsPage } from "@/lib/types";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
@@ -16,7 +16,7 @@ export async function GET(req: NextRequest) {
     }
 
     const posts = await prisma.post.findMany({
-      include: PostDataInclude,
+      include: getPostDataInclude(user.id),
       orderBy: { createdAt: "desc" },
       take: pageSize + 1,
       cursor: cursor ? { id: cursor } : undefined,
