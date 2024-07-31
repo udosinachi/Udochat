9622255b78a1ce43f584a74da4279939acc71022
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2024-07-31 09:37:02 +0100

    React Query setup

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,22 +1,13 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
-import Post from "@/components/posts/Post";
 import TrendsSidebar from "@/components/TrendsSideBar";
-import prisma from "@/lib/prisma";
-import { PostDataInclude } from "@/lib/types";
+import ForYouFeed from "./ForYouFeed";
 
 export default async function Home() {
-  const posts = await prisma.post.findMany({
-    include: PostDataInclude,
-    orderBy: { createdAt: "desc" },
-  });
-
   return (
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        {posts.map((post) => (
-          <Post key={post.id} post={post} />
-        ))}
+        <ForYouFeed />
       </div>
       <TrendsSidebar />
     </main>
