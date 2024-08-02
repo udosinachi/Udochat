01daf40fde6d1ac76769cc799e7e0e4467f0b89a
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2024-08-02 08:52:03 +0100

    Added Link to hashtags and username when mentioned

--- src/app/(main)/users/[username]/page.tsx
+++ src/app/(main)/users/[username]/page.tsx
@@ -12,6 +12,7 @@ import { cache } from "react";
 import UserPosts from "./UserPosts";
 import TrendsSidebar from "@/components/TrendsSideBar";
 import FollowerCount from "@/components/FollowerCount";
+import Linkify from "@/components/Linkify";
 
 interface PageProps {
   params: { username: string };
@@ -122,9 +123,11 @@ async function UserProfile({ user, loggedInUserId }: UserProfileProps) {
       {user.bio && (
         <>
           <hr />
-          <div className="overflow-hidden whitespace-pre-line break-words">
-            {user.bio}
-          </div>
+          <Linkify>
+            <div className="overflow-hidden whitespace-pre-line break-words">
+              {user.bio}
+            </div>
+          </Linkify>
         </>
       )}
     </div>
