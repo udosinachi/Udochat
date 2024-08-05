7ae3593706e1259b6637924d9e3bef9d4816155c
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2024-08-05 17:35:00 +0100

    Profile edit and profile picture working

--- src/app/(main)/users/[username]/page.tsx
+++ src/app/(main)/users/[username]/page.tsx
@@ -13,6 +13,7 @@ import UserPosts from "./UserPosts";
 import TrendsSidebar from "@/components/TrendsSideBar";
 import FollowerCount from "@/components/FollowerCount";
 import Linkify from "@/components/Linkify";
+import EditProfileButton from "./EditProfileButton";
 
 interface PageProps {
   params: { username: string };
@@ -115,7 +116,7 @@ async function UserProfile({ user, loggedInUserId }: UserProfileProps) {
           </div>
         </div>
         {user.id === loggedInUserId ? (
-          <Button>Edit profile</Button>
+          <EditProfileButton user={user} />
         ) : (
           <FollowButton userId={user.id} initialState={followerInfo} />
         )}
