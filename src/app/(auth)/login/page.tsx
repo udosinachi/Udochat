d59702b04cb441c77be166f5cbf2014628d30da8
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2024-08-20 16:52:15 +0100

    Google SignIn works

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,6 +2,7 @@ import loginImage from "@/assets/login-image.jpg";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import GoogleSignInButton from "./GoogleSignInButton";
 import LoginForm from "./LoginForm";
 
 export const metadata: Metadata = {
@@ -12,9 +13,15 @@ export default function Page() {
   return (
     <main className="flex h-screen items-center justify-center p-5">
       <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
-        <div className="w-full space-y-10 overflow-y-auto px-5 py-10 md:w-1/2 md:p-10">
-          <h1 className="text-center text-3xl font-bold">Login to Udochat</h1>
+        <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
+          <h1 className="text-center text-3xl font-bold">Login to bugbook</h1>
           <div className="space-y-5">
+            <GoogleSignInButton />
+            <div className="flex items-center gap-3">
+              <div className="h-px flex-1 bg-muted" />
+              <span>OR</span>
+              <div className="h-px flex-1 bg-muted" />
+            </div>
             <LoginForm />
             <Link href="/signup" className="block text-center hover:underline">
               Don&apos;t have an account? Sign up
